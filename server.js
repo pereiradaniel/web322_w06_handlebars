1469a5ef13f4b4d47de9d82c2d4495e45fb8716b
pereiradaniel <danielpaulpereira@protonmail.com>
2021-02-18 17:24:36 -0500

    Add json with html route example.

--- server.js
+++ server.js
@@ -19,5 +19,41 @@ app.get("/about", function(req,res){
     res.sendFile(path.join(__dirname, "/views/about.html"));
 });
 
+// send json data and display in html
+app.get("/viewData", function(req,res){
+
+    var someData = {
+        name: "John",
+        age: 23,
+        occupation: "developer",
+        company: "Scotiabank"
+    };
+
+    var htmlString = "<!doctype html>" + 
+                    "<html>" +
+                        "<head>" + 
+                            "<title>" + "View Data" + "</title>" +
+                        "</head>" +
+                        "<body>" + 
+                            "<table border='1'>" + 
+                                "<tr>" + 
+                                    "<th>" + "Name" + "</th>" + 
+                                    "<th>" + "Age" + "</th>" + 
+                                    "<th>" + "Occupation" + "</th>" + 
+                                    "<th>" + "Company" + "</th>" + 
+                                "</tr>" + 
+                                "<tr>" + 
+                                    "<td>" + someData.name + "</td>" + 
+                                    "<td>" + someData.age + "</td>" + 
+                                    "<td>" + someData.occupation + "</td>" + 
+                                    "<td>" + someData.company + "</td>" + 
+                                "</tr>" + 
+                            "</table>" + 
+                        "</body>" + 
+                    "</html>";
+
+    res.send(htmlString);
+});
+
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
